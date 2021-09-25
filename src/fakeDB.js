1e34caa20d08ec498e8e0131eb89220cbd52638f
Ibukun <89906543+Aibeekaysart@users.noreply.github.com>
2021-09-25 16:24:38 +0100

    First Comit

---
+++ src/fakeDB.js
@@ -0,0 +1,17 @@
+exports.fakeDB = [
+    {
+        id:0,
+        email:'ibukun.sanni@gmail.com',
+        password: '$2a$10$ZqewUIvGHc32jisPBHCXUeUay8IMh4fVa4c59NtpS1Z3k/3sDiM62'// Password: test
+    },
+    {
+        id:1,
+        email:'taiwo.ola@gmail.com',
+        password: '$2a$10$ZqewUIvGHc32jisPBHCXUeUay8IMh4fVa4c59NtpS1Z3k/3sDiM62'// Password: test
+    },
+    {
+        id:2,
+        email:'obi.nwankwo@gmail.com',
+        password: '$2a$10$ZqewUIvGHc32jisPBHCXUeUay8IMh4fVa4c59NtpS1Z3k/3sDiM62'// Password: test
+    }
+];
---
+++ src/index.js
@@ -0,0 +1,107 @@
+require('dotenv/config');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const {verify} = require('jsonwebtoken');
+const {hash, compare} = require('bcryptjs');
+const { fakeDB } = require('./fakeDB.js');
+const { 
+    createAccessToken, 
+    createRefreshToken,
+    sendAccessToken,
+    sendRefreshToken
+ } = require('./tokens.js');
+
+
+//1. Register a user
+//2. Login a user
+//3. Logout a user
+//4. Setup a protected route
+//5. Get a new access token with a refresh token
+
+const server = express();
+
+//Use express middleware for easier cookie handling 
+server.use(cookieParser());
+
+server.use(
+    cors({
+    origin:'http://localhost:3000',
+    credentials: true,
+}),
+);
+
+// Needed to be able to read body data
+server.use(express.json());// to support JSON encoded bodies
+server.use(express.urlencoded({extended: true}))// support URL-encoded bodies
+
+
+
+// 1. Register a user
+server.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+        // 1. Check if user exist
+        const user = fakeDB.find(user => user.email === email);
+        if (user) throw new Error('User already exists');
+
+        // 2. If not user exist, hash the password
+        const hashedPassword  = await hash(password,10);
+        
+        //3. Insert the user in "database"
+        fakeDB.push({
+            id: fakeDB.length,
+            email,
+            password: hashedPassword
+        });
+        res.send({message: 'User Created'});
+
+        console.log(fakeDB)
+
+    }catch(err){
+        res.send({
+            error: `${err.message}`,
+
+        });
+        
+    }
+});
+
+// 2. Login a user
+server.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        //1 . Find user in "database". If user does not exist, send error
+        const user = fakeDB.find(user => user.email === email);
+        if(!user) throw new Error('User does not exist');
+        
+        //2. Compare crypted password and see if it checks out. Send error if not
+        const valid = await compare(password, user.password);
+        if (!valid) throw new Error ("Password not correct");
+        
+        //3. Create Refresh- and Accesstoken
+        const accesstoken = createAccessToken(user.id);
+        const refreshtoken = createRefreshToken(user.id);
+
+        // 4. Put the refreshtoken in the "database"
+        user.refreshtoken = refreshtoken;
+        console.log(fakeDB);
+
+        //5. Send token. Rfreshtoken as a cookie and accesstoken as regular response
+        sendRefreshToken(res, refreshtoken);
+        sendAccessToken(res, req, accesstoken);
+    
+    } catch(err) {
+        res.send({
+            error:`${err.message}`,
+        });
+
+    }
+
+});
+
+
+server.listen(process.env.PORT, () =>
+  console.log(`Server listening on port ${process.env.PORT}!`),
+);
